a03d21a83ec1bde328781cbe65e68b7fb273e84c
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-06-12 13:04:56 +0300

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Ali Emre Değirmenci on 12.06.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
